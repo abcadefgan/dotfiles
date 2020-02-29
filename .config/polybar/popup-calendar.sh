2f12a4e6b20d14fa50387ff21ca469ee4eb7c169
abcadefgan <andars@gmail.com>
2020-02-29 10:07:01 +0100

    added polybar calendar tweaks

--- .config/polybar/popup-calendar.sh
+++ .config/polybar/popup-calendar.sh
@@ -1,26 +1,41 @@
 #!/bin/sh
 
-WIDTH=270
-HEIGHT=274
-BOTTOM=false
-DATE="$(date +"  %a, %d %b  %H:%M ")"
+BAR_HEIGHT=24  # polybar height
+BORDER_SIZE=0  # border size from your wm settings
+YAD_WIDTH=222  # 222 is minimum possible value
+YAD_HEIGHT=193 # 193 is minimum possible value
+DATE="$(date +"%d")"
 
 case "$1" in
-    --popup)
-        eval "$(xdotool getmouselocation --shell)"
+--popup)
+    if [ "$(xdotool getwindowfocus getwindowname)" = "yad-calendar" ]; then
+        exit 0
+    fi
 
-        if [ $BOTTOM = true ]; then
-            : $(( pos_y = Y - HEIGHT - 20 ))
-            : $(( pos_x = X - (WIDTH / 2) ))
-        else
-            : $(( pos_y = Y + 20 ))
-            : $(( pos_x = X - (WIDTH / 2) ))
-        fi
+    eval "$(xdotool getmouselocation --shell)"
+    eval "$(xdotool getdisplaygeometry --shell)"
 
-        urxvt -geometry 38x18+$pos_x+$pos_y --title calendar -e dialog --no-shadow --no-lines --no-cancel --week-start 1 --clear --calendar "" 1 40
+    # X
+    if [ "$((X + YAD_WIDTH / 2 + BORDER_SIZE))" -gt "$WIDTH" ]; then #Right side
+        : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+    elif [ "$((X - YAD_WIDTH / 2 - BORDER_SIZE))" -lt 0 ]; then #Left side
+        : $((pos_x = BORDER_SIZE))
+    else #Center
+        : $((pos_x = X - YAD_WIDTH / 2))
+    fi
 
-        ;;
-    *)
-        echo "$DATE"
-        ;;
+    # Y
+    if [ "$Y" -gt "$((HEIGHT / 2))" ]; then #Bottom
+        : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
+    else #Top
+        : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
+    fi
+
+    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+        --width=$YAD_WIDTH --height=$YAD_HEIGHT --posx=$pos_x --posy=$pos_y \
+        --title="yad-calendar" --borders=0 >/dev/null &
+    ;;
+*)
+    echo "$DATE"
+    ;;
 esac

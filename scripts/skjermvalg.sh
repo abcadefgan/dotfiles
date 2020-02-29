b7ec32e309c9ea42272e7f43338b51f1434704de
abcadefgan <andars@gmail.com>
2020-02-29 10:09:55 +0100

    various updates

--- scripts/skjermvalg.sh
+++ scripts/skjermvalg.sh
@@ -2,12 +2,16 @@
 
 skjermer="berbar\nberbardualHDMI\nHDMI\nrotVenstre\nrotHøyre"
 
-skjermvalg=$(echo -e "$skjermer" | dmenu -i -fn "Inconsolata-8" -sb "#237986")
+skjermvalg=$(echo -e "$skjermer" | rofi -dmenu -i -fn "Inconsolata-8" -theme eighties-dark.rasi)
 
 case "$skjermvalg" in
 	berbar) xrandr --output LVDS1 --primary --mode "1366x768" --output HDMI1 --off && pacmd set-card-profile 0 output:analog-stereoi & xautolock -enable;;
+	
 	berbardualHDMI) xrandr --output LVDS1 --auto --output HDMI1 --auto;;
+
 	HDMI) xrandr --output HDMI1 --mode "1920x1080" -r 60 --output LVDS1 --off && pacmd set-card-profile 0 output:hdmi-stereo && xautolock -disable;;
+	
 	rotVenstre) xrandr --output LVDS1 --rotate left;;
+	
 	rotHøyre) xrandr --output LVDS1 --rotate right;;
 esac
